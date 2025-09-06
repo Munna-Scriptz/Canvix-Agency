06506fedcddc6dfd1ffc215ebe8b9b0546ffc73e
Munna-Scripts <munnascriptz@gmail.com>
2025-09-06 16:25:42 +0600

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <>
+      <nav>
+        <div className="container">
+          asasd
+        </div>
+      </nav>
+    </>
+  )
+}
+
+export default Navbar
