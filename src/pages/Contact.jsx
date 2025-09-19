d0a8942e9c1ab1da4222259fb357b9c9574e69ca
Munna-Scripts <munnascriptz@gmail.com>
2025-09-19 15:06:29 +0600

    Contact Page Complete

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import LetsTalk from '../components/Contact/LetsTalk'
+import Location from '../components/Contact/Location'
 
 const Contact = () => {
   return (
     <>
         <LetsTalk/>
+        <Location/>
     </>
   )
 }
